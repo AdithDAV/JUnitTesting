6b0779bf89c82ce5010c1a88def92bb19f07db6b
Adith19 <56405876+Adith19@users.noreply.github.com>
2021-04-17 12:26:35 +0530

    JUnitTest

--- src/UnitTest/AllTests.java
+++ src/UnitTest/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({testAddNumber.class, testaddString.class})
+@SuiteClasses({testAddNumber.class,testaddString.class})
 public class AllTests {
 
 }
